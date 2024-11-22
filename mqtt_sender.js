2cc911caa0601ede4b7b26aa371420c374305b86
victor3d <59659352+victorsehested@users.noreply.github.com>
2024-11-22 12:09:43 +0100

    sd

--- mqtt_sender.js
+++ mqtt_sender.js
@@ -1,10 +1,11 @@
-function sender(a,b,c,motionZ) {
+function sender(a,b,c,motionZ,string) {
   besked = {
     from:afsenderID, 
     val1:a,
     val2:b,
     val3:c,
     val4:motionZ
+    string:string
   };
    client.publish(topic, JSON.stringify(besked));
 }
