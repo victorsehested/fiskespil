e12224770b77bd8634787c43f45adf7615d9692d
victor3d <59659352+victorsehested@users.noreply.github.com>
2024-11-29 09:46:41 +0100

    sadas

--- bobler2.js
+++ bobler2.js
@@ -21,49 +21,21 @@ function setup() {
 }
 
 function draw() {
-  //background(0, 0, 0); // transperant baggrund for fade-effect
+  //background(0, 0, 0);
+  textSize(50); 
+  fill(255); 
+  textAlign(CENTER);
+  text('PUST', width/2, height/2+20);
   
+  imageMode(CENTER);
+  image(img, width/2, height/2, 200, 200);
+  
+  imageMode(CORNER);
+  
+  
+ 
   // indlæs mikrofon level
-  let vol = amplitude.getLevel();
-  console.log(vol);
-  /*
-  // Check if volume exceeds threshold (for detecting blow)
-  if (vol > 0.1) {
-    let bubbleSize = map(vol, 0, 1, 50, 150); // lydvolumen mappes til bubblesize
-    bubbles.push(new Bubble(random(width), height, bubbleSize)); // bobler starter random x i bunden af skærm
-  }
+  sender(amplitude.getLevel());
   
-  // Display and move each bubble
-  for (let i = bubbles.length - 1; i >= 0; i--) {
-    bubbles[i].move();
-    bubbles[i].display();
-    
-    // bobler væk fra skærmen fjernes 
-    if (bubbles[i].y < -bubbles[i].size) {
-      bubbles.splice(i, 1); //  bubbles slettes fra array
-    }
-  }
-}
 
-// Bubble class to create each bubble
-class Bubble {
-  constructor(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.ySpeed = random(-1, -3); // flyder op fart
-    this.xSpeed = random(-1, 1);  // horisontal fart
-  }
-  
-  // boblen bevæges
-  move() {
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
-  }
-  
-  // boblen tegnes
-  display() {
-    imageMode(CENTER);
-    image(img, this.x, this.y, this.size, this.size);
-  }*/
 }
